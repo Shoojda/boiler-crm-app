e017deb98684c385315b34be50534df1b683e3f2
Shoojda <shoojda1@gmail.com>
2025-07-20 23:13:15 +0200

    Fix syntax error in app.js

--- server/app.js
+++ server/app.js
@@ -9,4 +9,5 @@ app.use(express.json());
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(Server running on port \));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
