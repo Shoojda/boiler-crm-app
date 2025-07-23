abcaa1255d44f2485096229e9a1b793ddc20aec6
Shoojda <shoojda1@gmail.com>
2025-07-24 01:14:35 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import clientsRouter from '../routes/clients.js';
+import clientsRouter from './routes/clients.js';
 
 
 dotenv.config();
